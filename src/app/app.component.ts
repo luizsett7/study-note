46d1fe1d5e8f413fbba00a74402f451cf0112f1c
Luiz Guilherme <luizsett7@gmail.com>
2022-10-23 23:15:52 -0300

    materialize-css, menu component and footer component inserted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import * as $ from 'jquery';
+import * as M from 'materialize-css';
+
+import { AfterViewInit, Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('mobile') sideNav?: ElementRef;
+
   title = 'StudyNote';
+
+  ngAfterViewInit():void{
+    let $sideNav = $('#mobile-demo');
+    //M.Sidenav.init(this.sideNav?.nativeElement);
+    M.Sidenav.init($sideNav);
+  }
 }
